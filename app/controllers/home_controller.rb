7a15abf580d1ecb3c4ef78a2c998c32cfbe459fc
Andrew Pepler <andrew@vertism.com>
2012-01-13 16:28:08 +0000

    stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,18 +5,50 @@ class HomeController < ApplicationController
   end
   
   def search
-    @imageURL = getimage(params[:search_term]).url
+    searchterms = params[:search_term]
+    @title = searchterms.split("+").join(" ") + " of the week"
+    @imageURL = getimageurl(searchterms)
+    if @imageURL.nil?
+      render :inline =>
+        "Sorry, there are no results for your search"
+    end
   end
   
-  def getimage(search)
+  def getimageurl(search)
     file = Rails.root.join('config','flickr.yml').to_s
     flickr = Flickr.new(file)
     
     tags = search.split("+").join(",")
     
-    photos = flickr.photos.search(:tags => tags, :min_upload_date =>'2012-01-09', :max_upload_date => '2012-01-14')
+    photos = flickr.photos.search(
+      :tags => tags,
+      :tag_mode => 'all',
+      :sort => 'interestingness-desc',
+      :content_type => 1,
+      :min_upload_date => getDate(1), :max_upload_date => getDate(2),
+      :per_page => 1
+    )
     
-    photos[0]
+    if photos.first.nil?
+      nil
+    else
+      photos.first.url
+    end
   end
-
+  
+  def getDate(type)
+    now = Time.now.to_date
+    minDate = now - 7 - now.cwday + 1
+    maxDate = minDate + 7
+    
+    case type
+    when 1 #min
+      minDate.strftime('%s')
+    when 2 #max
+      maxDate.strftime('%s')
+    else
+      nil
+    end
+  end
+  
 end
