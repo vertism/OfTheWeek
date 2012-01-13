2b243c9e66332bb67ea76bb6149db3db4de9c709
Andrew Pepler <andrew@vertism.com>
2012-01-13 13:05:48 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfTheWeek::Application.config.secret_token = '08082dff348f77a025cca3fad2c12dcdbbe237d0dd8b018ae28bfd99dd0e3142d4ac3b98d1d8182a50ba53213e1221effbcd582ba4be43c6532122e82014e6d2'
